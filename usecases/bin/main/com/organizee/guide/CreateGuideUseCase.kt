1e450ee8bcff3be8b6a15c620389ba6f4bf25846
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-07-19 22:05:30 -0300

    chore: remove bin folders

--- usecases/bin/main/com/organizee/guide/CreateGuideUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.organizee.guide
-
-import com.organizee.Guide
-import com.organizee.Usecase
-import com.organizee.guide.commands.NewGuideCommand
-
-interface CreateGuideUseCase : Usecase<NewGuideCommand, Guide> {
-
-}
