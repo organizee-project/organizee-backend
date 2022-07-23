6f73d2b9243b0edb5ed1abd1ad67452e7d901e2e
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-07-23 15:08:21 -0300

    refactor: user creation validation

--- usecases/src/main/kotlin/com/organizee/user/impl/CreateUserUseCaseImpl.kt
+++ usecases/src/main/kotlin/com/organizee/user/impl/CreateUserUseCaseImpl.kt
@@ -8,6 +8,15 @@ import com.organizee.user.commands.NewUserCommand
 class CreateUserUseCaseImpl(private val userService: UserService) : CreateUserUseCase {
     override fun execute(input: NewUserCommand): User {
 
+        val userByEmail = userService.findByEmail(input.email)
+        if (userByEmail != null)
+            throw RuntimeException("Email already taken")
+
+        val userByUsername = userService.findByUsername(input.username)
+        if (userByUsername != null)
+            throw RuntimeException("Username already taken")
+
+
         val user = User.createNormalUser(
             email = input.email,
             name = input.name,
