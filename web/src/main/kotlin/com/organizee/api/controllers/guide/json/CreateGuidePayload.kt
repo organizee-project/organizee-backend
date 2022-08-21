f4852e07ef2c2c4dca7a1763926344eaa5aaf7d2
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-08-21 17:05:52 -0300

    feature: adding validation to create guide resource

--- web/src/main/kotlin/com/organizee/api/controllers/guide/json/CreateGuidePayload.kt
+++ web/src/main/kotlin/com/organizee/api/controllers/guide/json/CreateGuidePayload.kt
@@ -2,13 +2,17 @@ package com.organizee.api.controllers.guide.json
 
 import com.organizee.guide.commands.NewCategoryCommand
 import com.organizee.guide.commands.NewGuideCommand
+import javax.validation.constraints.NotBlank
 
 data class CreateGuidePayload(
+    @field:NotBlank(message = "Title is mandatory")
     val title: String,
+    @field:NotBlank(message = "Subtitle is mandatory")
     val subtitle: String,
+    @field:NotBlank(message = "Content is mandatory")
     val content: String,
     val categories: List<CategoryPayload> = emptyList(),
-    val isPrivate: Boolean
+    val isPrivate: Boolean = false
 ) {
     fun toUseCaseInput() = NewGuideCommand(
         title = title,
