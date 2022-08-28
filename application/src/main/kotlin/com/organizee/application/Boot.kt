689f6264871e3ad51f42bad2f803f96af20e4afe
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-08-28 14:07:29 -0300

    chore: updating boot configurations

--- application/src/main/kotlin/com/organizee/application/Boot.kt
+++ application/src/main/kotlin/com/organizee/application/Boot.kt
@@ -3,10 +3,12 @@ package com.organizee.application
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import java.util.*
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.organizee"])
 @EnableJpaRepositories("com.organizee.adapter.db.repositories")
 @EntityScan("com.organizee.adapter.db.entities")
 class Boot {
