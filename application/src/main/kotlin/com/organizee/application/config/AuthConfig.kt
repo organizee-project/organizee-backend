5728d48fc0780e1de0b892675889b003100aa1cf
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-09-24 11:05:12 -0300

    feature: updating authorized endpoints

--- application/src/main/kotlin/com/organizee/application/config/AuthConfig.kt
+++ application/src/main/kotlin/com/organizee/application/config/AuthConfig.kt
@@ -1,8 +1,11 @@
 package com.organizee.application.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
 
 class AuthConfig {
 }
@@ -14,9 +17,17 @@ class WebSecurityConfiguration : WebSecurityConfigurerAdapter() {
         http.authorizeRequests()
             .antMatchers("/swagger-ui/**").permitAll()
             .antMatchers("/v1/users/**").permitAll()
+            .antMatchers("/v1/search/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/v1/guides/**").permitAll()
             .anyRequest().authenticated().and().csrf().disable()
 
         http.oauth2ResourceServer()
             .jwt()
+    }
+
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers(
+            "/swagger-ui/**",
+        )
     }
 }
