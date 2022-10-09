a4ff44ef660149ca90ccafff2b87ada00f80ed5f
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-10-09 14:39:32 -0300

    feature: adding remove saved guide resource

--- boundaries/src/main/kotlin/com/organizee/boundaries/db/services/SavedService.kt
+++ boundaries/src/main/kotlin/com/organizee/boundaries/db/services/SavedService.kt
@@ -3,5 +3,7 @@ package com.organizee.boundaries.db.services
 import com.organizee.domain.guide.Saved
 
 interface SavedService {
+    fun findSavedOrThrow(userId: String, slug: String): Saved
     fun findSaved(userId: String, slug: String): Saved?
+    fun remove(saved: Saved)
 }
