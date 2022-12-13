14a1c7df2177d1ae57d0254265f162baf637aaef
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2022-12-13 08:23:40 -0300

    feature: adding following list to logged resource

--- usecases/src/main/kotlin/com/organizee/usecases/user/impl/GetLoggedPerfilUseCaseImpl.kt
+++ usecases/src/main/kotlin/com/organizee/usecases/user/impl/GetLoggedPerfilUseCaseImpl.kt
@@ -19,7 +19,23 @@ class GetLoggedPerfilUseCaseImpl(
                 imgUrl = user.imgUrl,
                 fullName = user.getFullName(),
                 username = user.username,
-                description = user.description
+                description = user.description,
+                following = user.following.map {
+                    UserPerfilUseCaseResponse(
+                        fullName = it.getFullName(),
+                        description = it.description,
+                        username = it.username,
+                        imgUrl = it.imgUrl
+                    )
+                },
+                followers = user.followers.map {
+                    UserPerfilUseCaseResponse(
+                        fullName = it.getFullName(),
+                        description = it.description,
+                        username = it.username,
+                        imgUrl = it.imgUrl
+                    )
+                },
             )
         )
 
